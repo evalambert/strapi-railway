2744f145bfc8d6b2d8ef68461c0dcec50f2ead51
evalambert <145194730+evalambert@users.noreply.github.com>
2025-03-04 17:47:57 +0100

    Fix jsconfig.json moduleResolution

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,22 +1,9 @@
 module.exports = [
+  'strapi::logger',
   'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'https:'],
-          'media-src': ["'self'", 'data:', 'blob:', 'https:'],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
+  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
-  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
